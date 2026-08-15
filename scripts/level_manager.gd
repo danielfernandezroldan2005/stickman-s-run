825ff6809b82a7702a589b666a2c82b9b46cb428
Daniel Fernández Roldán <danielfernandezroldan2005@gmail.com>
2026-08-15 17:49:14 +0200

    Made an infinite system of platforms.

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -1,9 +1,26 @@
 extends Node
 
 @export var platform_template: PackedScene
+@export var player: CharacterBody2D
+
+var last_spawn_x: float = 0.0
+var current_y: float = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for i in range(5):
+		spawn_platform()
+		
+func spawn_platform() -> void:
 	var new_platform = platform_template.instantiate()
-	new_platform.position = Vector2(0, 100)
-	add_child(new_platform)
+	new_platform.position = Vector2(last_spawn_x, current_y)
+	add_child(new_platform)	
+	
+	last_spawn_x += randf_range(150.0,250.0)
+	current_y += randf_range(-40.0, 40.0)
+
+func _process(delta: float) -> void:
+	if player != null and player.global_position.x > last_spawn_x - 500.0:
+		spawn_platform()
+	
+	
